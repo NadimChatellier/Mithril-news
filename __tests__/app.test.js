f18abb4a3a1658aaebd27771a211450442e605ea
Mithrildagger <nadimchatellier@gmail.com>
2024-11-25 13:31:24 +0000

    Working my way out of setup hell in q3

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,15 +2,38 @@ const endpointsJson = require("../endpoints.json");
 /* Set up your test imports here */
 const app = require("../app")
 const request = require("supertest");
+const {testData} = require("../index")
+const {topicData, userData, articleData, commentData} = testData
+const db = require("../db/connection")
+const seed = require("../db/seeds/seed");
 /* Set up your beforeEach & afterAll functions here */
+
+beforeEach(() => seed({topicData, userData, articleData, commentData}))
+
+afterAll(() => db.end());
 
 describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
     return request(app)
       .get("/api")
       .expect(200)
-      .then(({ body: { endpoints } }) => {
-        expect(endpoints).toEqual(endpointsJson);
+      .then(({ body } ) => {
+        expect(body).toEqual(endpointsJson);
+      });
+  });
+});
+
+describe("GET /api/topics", () => {
+  test("200: Responds with an array of topic objects with expected keys and values", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        body.forEach(element => {  
+          expect(Object.keys(element)).toEqual([ 'slug', 'description' ]);
+          expect(typeof element.description).toEqual('string');
+          expect(typeof element.slug).toEqual('string');
+        });
       });
   });
 });
--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const endpoints = require("./index")
+const {endpoints, testData, devData} = require("./index");
 const express = require("express");
 const fs = require("fs/promises");
 const app = express();
+const {getTopics} = require("./controllers")
+const db = require('./db/connection');
 
 
 app.get('/api', (req, res) => {
     res.status(200).send(endpoints)
 })
 
+app.get('/api/topics', getTopics)
 
 module.exports = app

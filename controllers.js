d9228bb9df4c92b74ab871cb26c8c885d6a6e780
Mithrildagger <nadimchatellier@gmail.com>
2024-11-25 16:15:19 +0000

    Completed 4

--- controllers.js
+++ controllers.js
@@ -1,16 +1,30 @@
-const {getTopicData} = require('./models')
+const {getTopicData, getArticleIdData} = require('./models')
 const app = require("./app");
 const db = require('./db/connection');
 
 function getTopics(req, res){
     getTopicData().then((result) => {
-        console.log(result)
         res.status(200).send(result)
     })
     .catch((err) => {
         console.log(err)
     })
 }
-module.exports = {getTopics}
+
+function getArticleId(req, res){
+    const id = req.params.article_id
+    getArticleIdData(id).then((result) =>{
+        res.status(200).send(result)
+    })
+    .catch((err) => {
+        if (err.code === '22003'){
+            res.status(404).send({error: 404, msg :`article ${id} does not exist`})
+        }
+        else {
+            res.status(400).send({error: 400, msg :`bad request`})
+        }
+    })
+}
+module.exports = {getTopics, getArticleId}
     
 
--- models.js
+++ models.js
@@ -6,4 +6,15 @@ function getTopicData(){
     })   
 }
 
-module.exports = {getTopicData}
+function getArticleIdData(id){
+    return db.query(`SELECT * FROM articles WHERE article_id = $1`, [id] ).then((res)=>{
+        if (res.rows.length === 0) {
+            // Handle 404 error when no article is found
+            return Promise.reject({ error: 404, message: `Article with id ${id} does not exist.` });
+        }
+        return res.rows[0]
+    })
+    
+}
+
+module.exports = {getTopicData, getArticleIdData}
